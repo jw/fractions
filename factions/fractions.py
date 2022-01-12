1ddfb9d8cfdf6aa0df526eacad25cc449033cdc6
Jan Willems <jw@elevenbits.com>
2022-01-12 16:58:46 +0100

    Added reduce and positive plus tests.

--- factions/fractions.py
+++ factions/fractions.py
@@ -1,10 +1,16 @@
+import math
+
+
 class Fraction:
     def __init__(self, numerator: int = 1, denominator: int = 1):
         self.numerator = numerator
         self.denominator = denominator
+        self._reduce()
 
     def plus(self, fraction: "Fraction"):
-        return Fraction(self.numerator + fraction.numerator, self.denominator)
+        nominator = self.numerator * fraction.denominator + fraction.numerator * self.denominator
+        denominator = self.denominator * fraction.denominator
+        return Fraction(nominator, denominator)
 
     def __eq__(self, other):
         if isinstance(other, Fraction):
@@ -13,3 +19,13 @@ class Fraction:
                 and self.denominator == other.denominator
             )
         return False
+
+    def __str__(self):
+        return f"{self.numerator} / {self.denominator}"
+
+    def _reduce(self):
+        g = math.gcd(self.numerator, self.denominator)
+        if self.denominator < 0:
+            g = -g
+        self.numerator //= g
+        self.denominator //= g
--- tests/test_fractions.py
+++ tests/test_fractions.py
@@ -3,3 +3,18 @@ from factions.fractions import Fraction
 
 def test_zero_plus_zero():
     assert Fraction(0).plus(Fraction(0)) == Fraction(0)
+    assert Fraction(0, 1).plus(Fraction(0, 1)) == Fraction(0)
+
+
+def test_positive_plus_same_denominator():
+    assert Fraction(1, 2).plus(Fraction(2, 2)) == Fraction(3, 2)
+
+
+def test_positive_plus_different_denominator():
+    assert Fraction(1, 2).plus(Fraction(2, 3)) == Fraction(7, 6)
+
+
+def test_reduce():
+    assert Fraction(42) == Fraction(42)
+    assert Fraction(1, 2) == Fraction(1, 2)
+    assert Fraction(6, 8) == Fraction(3, 4)
