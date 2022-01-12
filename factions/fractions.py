3ce5d8507dbafe716a86c4d18e6d97cc998d9630
Jan Willems <jw@elevenbits.com>
2022-01-12 07:56:43 +0100

    Pre-commit hooks and first run.

--- factions/fractions.py
+++ factions/fractions.py
@@ -1,6 +1,4 @@
-
-class Fraction():
-
+class Fraction:
     def __init__(self, numerator: int = 1, denominator: int = 1):
         self.numerator = numerator
         self.denominator = denominator
@@ -10,5 +8,8 @@ class Fraction():
 
     def __eq__(self, other):
         if isinstance(other, Fraction):
-            return self.numerator == other.numerator and self.denominator == other.denominator
+            return (
+                self.numerator == other.numerator
+                and self.denominator == other.denominator
+            )
         return False
